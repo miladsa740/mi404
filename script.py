b6b23b4993b7342e5d67bb541447605ecf950bbb
milad <117509045+miladsa740@users.noreply.github.com>
2025-12-08 11:23:57 +0330

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import requests
 
 def fetch_servers():
-    url1 = "https://raw.githubusercontent.com/4n0nymou3/multi-proxy-config-fetcher/refs/heads/main/configs/proxy_configs.txt"
+    url1 = "https://raw.githubusercontent.com/4n0nymou3/multi-proxy-config-fetcher/refs/heads/m***/configs/proxy_configs.txt"
     url2 = "https://raw.githubusercontent.com/AvenCores/goida-vpn-configs/refs/heads/main/githubmirror/1.txt"
     url3 = "https://raw.githubusercontent.com/miladsa740/blackbird/refs/heads/main/config.txt" # اضافه کردن لینک سوم
 
