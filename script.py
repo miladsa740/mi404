d16b5cd46d2ae547c91e33fbb15a0029443c2c39
milad <117509045+miladsa740@users.noreply.github.com>
2026-07-16 09:37:56 +0330

    Update URL and limit values in script.py
    Replaced a URL in the list with a placeholder and updated the limit for the fourth link.

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ def fetch_servers():
     urls = [
         "https://mi7540.blkbmisa.dpdns.org/my-milisa?sub=mi7540",
         "https://n1m.novacell95.qzz.io/m1outlook?sub=m1u",
-        "https://raw.githubusercontent.com/LimeHi/LimeVPN/main/LimeVPN.txt",
+        "******",
         "https://raw.githubusercontent.com/justVisiting992/xray-Config-Collector/main/vless_iran.txt",
         "https://raw.githubusercontent.com/miladsa740/blackbird/refs/heads/main/config.txt"
     ]
@@ -18,7 +18,7 @@ def fetch_servers():
         None,  # لینک اول
         300,   # لینک دوم
         200,   # لینک سوم
-        150,   # لینک چهارم
+        100,   # لینک چهارم
         100    # لینک پنجم
     ]
 
